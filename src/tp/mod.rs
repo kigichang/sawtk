311c9c92961117a9372d01d0007a6ed5d39cd304
kigi <kigi.chang@gmail.com>
2019-11-25 09:17:52 +0800

    use invalid_transaction!.

--- src/tp/mod.rs
+++ src/tp/mod.rs
@@ -36,10 +36,11 @@ impl States {
 
     pub fn to_message(&self, address: &str, msg: &mut dyn protobuf::Message) -> Result<(), ApplyError> {
         let bytes = self.data.get(address).ok_or(
-            //ApplyError::InvalidTransaction(format!("{} not found", address))
             invalid_transaction!("{} not found", address)
         )?;
-        msg.merge_from_bytes(bytes).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))?;
+        msg.merge_from_bytes(bytes).map_err(|e| 
+            invalid_transaction!("{}", e)
+        )?;
         Ok(())
     }
 }
@@ -60,7 +61,7 @@ impl From<Vec<(String, Vec<u8>)>> for States {
 // -----------------------------------------------------------------------------
 
 pub fn get_state_entries(ctx: &dyn TransactionContext, addresses: Vec<String>) -> Result<(States), ApplyError> {
-    let result = ctx.get_state_entries(&addresses).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))?;
+    let result = ctx.get_state_entries(&addresses).map_err(|e| invalid_transaction!("{}", e))?;
     Ok(States::from(result))
 }
 
@@ -71,11 +72,11 @@ pub fn get_state_entry<T: protobuf::Message>(ctx: &dyn TransactionContext, addre
 
 
 pub fn delete_state_entries(ctx: &dyn TransactionContext, addresses: &[String]) -> Result<Vec<String>, ApplyError> {
-    ctx.delete_state_entries(addresses).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))
+    ctx.delete_state_entries(addresses).map_err(|e| invalid_transaction!("{}", e))
 }
 
 pub fn delete_state_entry(ctx: &dyn TransactionContext, address: &str) -> Result<Option<String>, ApplyError> {
-    ctx.delete_state_entry(address).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))
+    ctx.delete_state_entry(address).map_err(|e| invalid_transaction!("{}", e))
 }
 
 
@@ -83,27 +84,27 @@ pub fn set_state_entries(ctx: &dyn TransactionContext, data: Vec<(String, &dyn M
     let mut entries: Vec<(String, Vec<u8>)> = Vec::new();
 
     for rs in data {
-        let bytes = rs.1.write_to_bytes().map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))?;
+        let bytes = rs.1.write_to_bytes().map_err(|e| invalid_transaction!("{}", e))?;
         entries.push((rs.0, bytes));
     }
 
-    ctx.set_state_entries(entries).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))
+    ctx.set_state_entries(entries).map_err(|e| invalid_transaction!("{}", e))
 }
 
 pub fn set_state_entry(ctx: &dyn TransactionContext, address: String, data: &dyn Message) -> Result<(), ApplyError> {
-    let bytes = data.write_to_bytes().map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))?;
-    ctx.set_state_entry(address, bytes).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))
+    let bytes = data.write_to_bytes().map_err(|e| invalid_transaction!("{}", e))?;
+    ctx.set_state_entry(address, bytes).map_err(|e| invalid_transaction!("{}", e))
 }
 
 pub fn add_event(ctx: &dyn TransactionContext, event_type: String, attributes: Vec<(String, String)>, data: &dyn Message) -> Result<(), ApplyError> {
-    let bytes = data.write_to_bytes().map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))?;
-    ctx.add_event(event_type, attributes, &bytes).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))
+    let bytes = data.write_to_bytes().map_err(|e| invalid_transaction!("{}", e))?;
+    ctx.add_event(event_type, attributes, &bytes).map_err(|e| invalid_transaction!("{}", e))
 }
 
 // -----------------------------------------------------------------------------
 
 pub fn to_tp_request(req: &TpProcessRequest) -> Result<TPRequest, ApplyError> {
-    protobuf::parse_from_bytes::<TPRequest>(&req.payload).map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))
+    protobuf::parse_from_bytes::<TPRequest>(&req.payload).map_err(|e| invalid_transaction!("{}", e))
 }
 
 // -----------------------------------------------------------------------------
@@ -116,157 +117,3 @@ impl Validate for TPRequest {
         Ok(())
     }
 }
-
-/*
-// -----------------------------------------------------------------------------
-pub struct Context<'a> {
-    ctx: &'a mut dyn TransactionContext,
-    origin: &'a TpProcessRequest,
-    cmd: i32,
-}
-
-impl<'a> Context<'a> {
-
-    pub fn command(&mut self) -> i32 {
-        self.cmd
-    }
-
-    pub fn get_origin_request(&mut self) -> &TpProcessRequest {
-        self.origin
-    }
-
-    pub fn get_signer_public_key(&mut self) -> &str {
-        self.origin.get_header().get_signer_public_key()
-    }
-
-    pub fn get_batcher_public_key(&mut self) -> &str {
-        self.origin.get_header().get_batcher_public_key()
-    }
-
-
-    pub fn get_state_entries(&mut self, addresses: Vec<String>) -> Result<(States), ApplyError> {
-        get_state_entries(self.ctx, addresses)
-    }
-
-    pub fn get_state_entry<T: protobuf::Message>(&mut self, address: &str) -> Result<T, ApplyError> {
-        get_state_entry(self.ctx, address)
-    }
-
-
-    pub fn delete_state_entries(&mut self, addresses: &[String]) -> Result<Vec<String>, ApplyError> {
-        delete_state_entries(self.ctx, addresses)
-    }
-
-    pub fn delete_state_entry(&mut self, address: &str) -> Result<Option<String>, ApplyError> {
-        delete_state_entry(self.ctx, address)
-    }
-
-
-    pub fn set_state_entries(&mut self, data: Vec<(String, &dyn Message)>) -> Result<(), ApplyError> {
-        set_state_entries(self.ctx, data)
-    }
-
-    pub fn set_state_entry(&mut self, address: String, data: &dyn Message) -> Result<(), ApplyError> {
-        set_state_entry(self.ctx, address, data)
-    }
-
-
-    pub fn add_event(&mut self, event_type: String, attributes: Vec<(String, String)>, data: &dyn Message) -> Result<(), ApplyError> {
-        add_event(self.ctx, event_type, attributes, data)
-    }
-
-}
-
-// -----------------------------------------------------------------------------
-
-type Constructor = Box<(dyn (Fn() -> Box<dyn Validate>) + 'static)>;
-
-type HandleFunc = Box<(dyn (Fn(&mut Context, &dyn Validate) -> Result<(), ApplyError>) + 'static)>;
-
-
-pub fn make_constructor<T: Validate>(f: impl Fn() -> Box<T> + 'static) -> Constructor {
-    Box::new( || -> Box <dyn Validate> { f() } ) as Constructor
-}
-
-pub fn make_handle_func<T: Validate>(f: impl Fn(&mut Context, &T) -> Result<(), ApplyError> + 'static) -> HandleFunc {
-    Box::new(|c: &mut Context, r: &dyn Validate| -> Result<(), ApplyError> { f(c, r) } ) as HandleFunc
-}
-
-pub struct Handler {
-    family_name: String,
-    family_versions: Vec<String>,
-    namespaces: Vec<Box<dyn Namespace>>,
-    handles: HashMap<i32, (HandleFunc, Constructor)>,
-}
-
-impl Handler {
-    pub fn new(family_name: &str, family_versions: &[String], namespaces: &[&str]) -> Self {
-        let all_ns: Vec<Box<dyn Namespace>> = namespaces.iter().map(|x| ns::new(x)).collect::<Vec<_>>();
-
-        Handler { 
-            family_name: family_name.to_string(),
-            family_versions: Vec::from(family_versions),
-            namespaces: all_ns,
-            handles: HashMap::new(),
-        }
-    }
-
-    pub fn add<T: Validate>(&mut self, cmd: i32, f: impl Fn(&mut Context, &T) -> Result<(), ApplyError> + 'static, c: impl Fn() -> Box<T> + 'static) {
-        self.handles.insert(cmd, (make_handle_func(f), make_constructor(c)));
-    }
-}
-
-impl fmt::Display for Handler {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, 
-            "family_name: {}, family_versions: {:?}, namespaces: {}", 
-            self.family_name, 
-            self.family_versions, 
-            self.namespaces.iter()
-                .map(|n| format!("{}", n))
-                .collect::<Vec<_>>()
-                .join(",")
-        )
-    }
-}
-
-impl TransactionHandler for Handler {
-    fn family_name(&self) -> String {
-        self.family_name.clone()
-    }
-
-    fn family_versions(&self) -> Vec<String> {
-        self.family_versions.clone()
-    }
-
-    fn namespaces(&self) -> Vec<String> {
-       self.namespaces.iter().map(|n| n.prefix().to_string()).collect::<Vec<_>>()
-    }
-
-    fn apply(&self, request: &TpProcessRequest, context: &mut dyn TransactionContext) -> Result<(), ApplyError> {
-        let req = to_tp_request(request)?;
-        let cmd = req.cmd;
-
-        let (h, c) = self.handles.get(&cmd).ok_or(ApplyError::InvalidTransaction(format!("unknown command {}", cmd)))?;
-        let new_req: Box<dyn Validate> = 
-            if req.payload.len() == 0 {
-                Box::new(req)
-            } else {
-                let mut tmp = c();
-                tmp.merge_from_bytes(&req.payload)
-                    .map_err(|e| ApplyError::InvalidTransaction(format!("{}", e)))?;
-                tmp
-            };
-
-        new_req.validate()?;
-
-        let mut ctx = Context {
-            ctx: context,
-            origin: request,
-            cmd: cmd,
-        };
-
-        h(&mut ctx, &*new_req)
-    }
-}
-*/
